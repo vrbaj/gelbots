435e13e595b4b47d004957c394b1176e3e71f8e3
xr72 <vrbik.vrbik@gmail.com>
2020-02-25 13:17:53 +0100

    Initial commit

---
+++ video_convertor.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import os
+import glob
+
+img_array = []
+files_to_proces = os.listdir(os.path.join("videos"))
+print(type(files_to_proces))
+files_to_proces.sort()
+print(files_to_proces)
+for idx, filename in enumerate(files_to_proces): # enumerate(os.listdir(os.path.join("videos"))):
+    print(filename)
+    img = cv2.imread(os.path.join("videos", str(filename)))
+
+    height, width, layers = img.shape
+    size = (width, height)
+    img_array.append(img)
+    img_array.append(img)
+    img_array.append(img)
+
+out = cv2.VideoWriter('crawling.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
