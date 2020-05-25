9b2c7de88d210ad2b6d2bb1d2370cbe961d0aad0
vrbaj <vrbik.vrbik@gmail.com>
2020-05-25 10:41:16 +0200

    laser window added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QComboBox,\
     QLineEdit, QPushButton, QFrame, QFileDialog, QCheckBox
 from PyQt5.QtCore import QSize, QRect, Qt, pyqtSignal
-from PyQt5.QtGui import QIntValidator, QPixmap, QDoubleValidator
+from PyQt5.QtGui import QIntValidator, QPixmap, QDoubleValidator, QFont
 import cv2
 import time, datetime
 import numpy as np
@@ -12,6 +12,103 @@ import CameraWorker
 import RaspiWorker
 import DiskCore
 import configparser
+
+
+class LaserSettingsWindow(QMainWindow):
+    change_params = pyqtSignal(int, int, int, int, int)
+
+    def __init__(self, pulse_number, on_time, off_time, x, y):
+        super(LaserSettingsWindow, self).__init__()
+        # set variables
+        # self.save_interval = interval
+        # self.save_namespace = namespace
+        # self.save_path = path
+
+        self.laser_pulse_n = pulse_number
+        self.laser_on_time = on_time
+        self.laser_off_time = off_time
+        self.laser_x_loc = x
+        self.laser_y_loc = y
+        # set window properties
+        self.setMinimumSize(QSize(250, 200))
+        self.setWindowTitle("Laser settings")
+
+        self.onlyInt = QIntValidator()
+
+        # LASER
+        # Create pulse number label
+        self.pulseNumberLabel = QLabel(self)
+        self.pulseNumberLabel.setGeometry(QRect(10, 0, 80, 20))
+        self.pulseNumberLabel.setText("Pulse n.:")
+
+        # Create pulse number input box
+        self.pulseNumberInput = QLineEdit(self)
+        self.pulseNumberInput.setGeometry(QRect(60, 0, 40, 20))
+        self.pulseNumberInput.setText(str(self.laser_pulse_n))
+        self.pulseNumberInput.setValidator(self.onlyInt)
+
+
+        # Create laser on label
+        self.laserOnLabel = QLabel(self)
+        self.laserOnLabel.setGeometry(QRect(10, 30, 80, 20))
+        self.laserOnLabel.setText("On.:")
+
+        # Create laser on input box
+        self.laserOnInput = QLineEdit(self)
+        self.laserOnInput.setGeometry(QRect(60, 30, 40, 20))
+        self.laserOnInput.setText(str(self.laser_on_time))
+        self.laserOnInput.setValidator(self.onlyInt)
+
+
+        # Create laser off label
+        self.laserOffLabel = QLabel(self)
+        self.laserOffLabel.setGeometry(QRect(10, 60, 80, 20))
+        self.laserOffLabel.setText("Off.:")
+
+        # Create laser off input box
+        self.laserOffInput = QLineEdit(self)
+        self.laserOffInput.setGeometry(QRect(60, 60, 40, 20))
+        self.laserOffInput.setText(str(self.laser_off_time))
+        self.laserOffInput.setValidator(self.onlyInt)
+
+        # Create laser x loc label
+        self.laserLocXLabel = QLabel(self)
+        self.laserLocXLabel.setGeometry(QRect(10, 90, 80, 20))
+        self.laserLocXLabel.setText("X:")
+
+        # Create laser x coordinate input box
+        self.laserCoordXInput = QLineEdit(self)
+        self.laserCoordXInput.setGeometry(QRect(60, 90, 40, 20))
+        self.laserCoordXInput.setValidator(self.onlyInt)
+        self.laserCoordXInput.setText(str(self.laser_x_loc))
+
+        # Create laser y loc label
+        self.laserLocYLabel = QLabel(self)
+        self.laserLocYLabel.setGeometry(QRect(10, 120, 80, 20))
+        self.laserLocYLabel.setText("Y:")
+
+        # Create laser y coordinate input box
+        self.laserCoordYInput = QLineEdit(self)
+        self.laserCoordYInput.setGeometry(QRect(60, 120, 40, 20))
+        self.laserCoordYInput.setValidator(self.onlyInt)
+        self.laserCoordYInput.setText(str(self.laser_y_loc))
+
+        # Apply button
+        self.validateButton = QPushButton(self)
+        self.validateButton.setGeometry(QRect(10, 150, 230, 40))
+        self.validateButton.setToolTip("Click to save settings")
+        self.validateButton.setFont(QFont('Times', 20))
+        self.validateButton.setText("Apply")
+        self.validateButton.clicked.connect(self.validate_settings)
+
+    def validate_settings(self):
+        self.laser_pulse_n = int(self.pulseNumberInput.text())
+        self.laser_on_time = int(self.laserOnInput.text())
+        self.laser_off_time = int(self.laserOffInput.text())
+        self.laser_x_loc = int(self.laserCoordXInput.text())
+        self.laser_y_loc = int(self.laserCoordYInput.text())
+        self.change_params.emit(self.laser_pulse_n, self.laser_on_time, self.laser_off_time, self.laser_x_loc,
+                                self.laser_y_loc)
 
 
 class VideoSettingsWindow(QMainWindow):
@@ -244,72 +341,6 @@ class ExampleWindow(QMainWindow):
         self.brightnessInput.setValidator(self.onlyDbl)
         self.brightnessInput.editingFinished.connect(self.brightness_edited)
 
-        # LASER
-        # Create pulse number label
-        self.pulseNumberLabel = QLabel(central_widget)
-        self.pulseNumberLabel.setGeometry(QRect(10, 30, 80, 31))
-        self.pulseNumberLabel.setText("Pulse n.:")
-
-        # Create pulse number input box
-        self.pulseNumberInput = QLineEdit(central_widget)
-        self.pulseNumberInput.move(60, 35)
-        self.pulseNumberInput.setFixedWidth(30)
-        self.pulseNumberInput.setText(str(self.laser_pulse_n))
-        self.pulseNumberInput.setValidator(self.onlyInt)
-        self.pulseNumberInput.editingFinished.connect(self.pulse_number_edited)
-
-        # Create laser on label
-        self.laserOnLabel = QLabel(central_widget)
-        self.laserOnLabel.setGeometry(QRect(120, 30, 80, 31))
-        self.laserOnLabel.setText("On.:")
-
-        # Create laser on input box
-        self.laserOnInput = QLineEdit(central_widget)
-        self.laserOnInput.move(155, 35)
-        self.laserOnInput.setFixedWidth(30)
-        self.laserOnInput.setText(str(self.laser_on_time))
-        self.laserOnInput.setValidator(self.onlyInt)
-        self.laserOnInput.editingFinished.connect(self.laser_on_edited)
-
-        # Create laser off label
-        self.laserOffLabel = QLabel(central_widget)
-        self.laserOffLabel.setGeometry(QRect(200, 30, 80, 31))
-        self.laserOffLabel.setText("Off.:")
-
-        # Create laser off input box
-        self.laserOffInput = QLineEdit(central_widget)
-        self.laserOffInput.move(235, 35)
-        self.laserOffInput.setFixedWidth(30)
-        self.laserOffInput.setText(str(self.laser_off_time))
-        self.laserOffInput.setValidator(self.onlyInt)
-        self.laserOffInput.editingFinished.connect(self.laser_off_edited)
-
-        # Create laser x loc label
-        self.laserLocXLabel = QLabel(central_widget)
-        self.laserLocXLabel.setGeometry(QRect(280, 30, 80, 31))
-        self.laserLocXLabel.setText("X:")
-
-        # Create laser x coordinate input box
-        self.laserCoordXInput = QLineEdit(central_widget)
-        self.laserCoordXInput.move(305, 35)
-        self.laserCoordXInput.setFixedWidth(30)
-        self.laserCoordXInput.setValidator(self.onlyInt)
-        self.laserCoordXInput.setText(str(self.laser_x_loc))
-        self.laserCoordXInput.editingFinished.connect(self.laser_x_loc_edited)
-
-        # Create laser y loc label
-        self.laserLocYLabel = QLabel(central_widget)
-        self.laserLocYLabel.setGeometry(QRect(350, 30, 80, 31))
-        self.laserLocYLabel.setText("Y:")
-
-        # Create laser y coordinate input box
-        self.laserCoordYInput = QLineEdit(central_widget)
-        self.laserCoordYInput.move(370, 35)
-        self.laserCoordYInput.setFixedWidth(30)
-        self.laserCoordYInput.setValidator(self.onlyInt)
-        self.laserCoordYInput.setText(str(self.laser_y_loc))
-        self.laserCoordYInput.editingFinished.connect(self.laser_y_loc_edited)
-
         # Create disk x loc label
         self.diskLocXLabel = QLabel(central_widget)
         self.diskLocXLabel.setGeometry(QRect(410, 30, 80, 31))
@@ -439,6 +470,13 @@ class ExampleWindow(QMainWindow):
         self.saveVideoButton.move(1300, 30)
         self.saveVideoButton.setFixedHeight(22)
         self.saveVideoButton.clicked.connect(self.save_video_settings)
+
+        # laser settings window
+        self.laserSettingsButton = QPushButton("Laser settings", self)
+        self.laserSettingsButton.setToolTip("Click to set laser settings")
+        self.laserSettingsButton.move(1300, 150)
+        self.laserSettingsButton.setFixedHeight(22)
+        self.laserSettingsButton.clicked.connect(self.show_laser_settings)
 
         # check box to move laser to desired position
         self.moveLaserCheckbox = QCheckBox(self)
@@ -615,7 +653,25 @@ class ExampleWindow(QMainWindow):
         # video settings window
         self.video_settings_window = VideoSettingsWindow(self.save_interval, self.save_namespace, self.save_path)
         self.video_settings_window.closed.connect(self.video_settings_close)
+
+        # laser settings window
+        self.laser_settings_window = LaserSettingsWindow(self.laser_pulse_n, self.laser_on_time, self.laser_off_time,
+                                                         self.laser_x_loc, self.laser_y_loc)
+        self.laser_settings_window.change_params.connect(self.laser_settings_changed)
         self.showMaximized()
+
+    def laser_settings_changed(self, n, on, off, x, y):
+        self.laser_pulse_n = n
+        self.laser_on_time = on
+        self.laser_off_time = off
+        self.laser_x_loc = x
+        self.laser_y_loc = y
+        self.config.set("laser", "pulses", self.laser_pulse_n)
+        self.config.set("laser", "on_time", self.laser_on_time)
+        self.config.set("laser", "off_time", self.laser_off_time)
+        self.config.set("laser", "x_loc", self.laser_x_loc)
+        self.config.set("laser", "y_loc", self.laser_y_loc)
+        self.update_config_file()
 
     def pulse(self):
         self.raspi_comm.requests_queue.append("a" + "," + str(self.sfl_light_on) + "," + str(self.sfl_pulse))
@@ -803,7 +859,6 @@ class ExampleWindow(QMainWindow):
             self.update_config_file()
 
     def video_settings_close(self, save_interval, save_namespace, save_path):
-        print(save_interval, save_namespace, save_path)
         self.config.set("video", "interval", save_interval)
         self.config.set("video", "namespace", save_namespace)
         self.config.set("video", "path", save_path)
@@ -812,6 +867,9 @@ class ExampleWindow(QMainWindow):
     def save_video_settings(self):
         self.video_settings_window.show()
         # print(self.video_settings_window.some_bullshit)
+
+    def show_laser_settings(self):
+        self.laser_settings_window.show()
 
     def steppers_x_edited(self):
         """Function to set steppers steps for manual control"""
