196675e3a7cf48b7b2a9bc22cf25ba51eef47941
vrbaj <vrbik.vrbik@gmail.com>
2020-10-20 19:49:37 +0200

    stamping added

--- main.py
+++ main.py
@@ -15,27 +15,41 @@ import configparser
 import keyboard
 
 
-
 class sflSettingsWindow(QMainWindow):
-    change_params = pyqtSignal(int, int, int, int, int, int)
+    change_params = pyqtSignal(int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int)
     pulse_signal = pyqtSignal()
     flush_switch_signal = pyqtSignal()
     light_switch_signal = pyqtSignal()
+    stamping_switch_signal = pyqtSignal(str)
     sfl_switch_signal = pyqtSignal()
-    #key_press_signal = pyqtSignal(int)
 
+    # key_press_signal = pyqtSignal(int)
 
-    def __init__(self, flush_on, flush_off, light_on, light_off, pulse, radius):
+    def __init__(self, flush_on, flush_off, light_on, light_off, pulse, radius,
+                 stamping_dx, stamping_dy, stamping_x_delay, stamping_y_delay, stamping_light_on,
+                 stamping_light_off, stamping_flush_on, stamping_flush_off, stamping_x_steps,
+                 stamping_y_steps, stamping_batch_size):
         super(sflSettingsWindow, self).__init__()
-
         self.sfl_flush_on = flush_on
         self.sfl_flush_off = flush_off
         self.sfl_light_on = light_on
         self.sfl_light_off = light_off
         self.sfl_pulse = pulse
         self.sfl_radius = radius
+        self.stamping_dx = stamping_dx
+        self.stamping_dy = stamping_dy
+        self.stamping_x_delay = stamping_x_delay
+        self.stamping_y_delay = stamping_y_delay
+        self.stamping_light_on = stamping_light_on
+        self.stamping_light_off = stamping_light_off
+        self.stamping_flush_on = stamping_flush_on
+        self.stamping_flush_off = stamping_flush_off
+        self.stamping_x_steps = stamping_x_steps
+        self.stamping_y_steps = stamping_y_steps
+        self.stamping_batch_size = stamping_batch_size
+
         # set window properties
-        self.setMinimumSize(QSize(250, 500))
+        self.setMinimumSize(QSize(450, 500))
         self.setWindowTitle("SFL settings")
 
         self.onlyInt = QIntValidator()
@@ -63,11 +77,11 @@ class sflSettingsWindow(QMainWindow):
 
         #
         self.sflRadiusLabel = QLabel(self)
-        self.sflRadiusLabel.setGeometry(QRect(100, 30, 50, 31))
+        self.sflRadiusLabel.setGeometry(QRect(100, 30, 50, 30))
         self.sflRadiusLabel.setText("Radius:")
 
         self.sflRadiusInput = QLineEdit(self)
-        self.sflRadiusInput.move(160, 30)
+        self.sflRadiusInput.move(140, 30)
         self.sflRadiusInput.setFixedWidth(30)
         self.sflRadiusInput.setValidator(self.onlyInt)
         self.sflRadiusInput.setText(str(self.sfl_radius))
@@ -104,6 +118,100 @@ class sflSettingsWindow(QMainWindow):
         self.sflFlushOffInput.setValidator(self.onlyInt)
         self.sflFlushOffInput.setText(str(self.sfl_flush_off))
 
+        self.stampingDxLabel = QLabel(self)
+        self.stampingDxLabel.setGeometry(QRect(200, 0, 100, 25))
+        self.stampingDxLabel.setText("Stamping dx:")
+        self.stampingDxInput = QLineEdit(self)
+        self.stampingDxInput.setGeometry(QRect(300, 0, 50, 25))
+        self.stampingDxInput.setValidator(self.onlyInt)
+        self.stampingDxInput.setText(str(self.stamping_dx))
+
+        self.stampingDyLabel = QLabel(self)
+        self.stampingDyLabel.setGeometry(QRect(200, 25, 100, 25))
+        self.stampingDyLabel.setText("Stamping dy:")
+        self.stampingDyInput = QLineEdit(self)
+        self.stampingDyInput.setGeometry(QRect(300, 25, 50, 25))
+        self.stampingDyInput.setValidator(self.onlyInt)
+        self.stampingDyInput.setText(str(self.stamping_dy))
+
+        self.stampingX_DelayLabel = QLabel(self)
+        self.stampingX_DelayLabel.setGeometry(QRect(200, 50, 100, 25))
+        self.stampingX_DelayLabel.setText("Stamping X delay:")
+        self.stampingX_DelayInput = QLineEdit(self)
+        self.stampingX_DelayInput.setGeometry(QRect(300, 50, 50, 25))
+        self.stampingX_DelayInput.setValidator(self.onlyInt)
+        self.stampingX_DelayInput.setText(str(self.stamping_x_delay))
+
+        self.stampingY_DelayLabel = QLabel(self)
+        self.stampingY_DelayLabel.setGeometry(QRect(200, 75, 100, 25))
+        self.stampingY_DelayLabel.setText("Stamping Y delay:")
+        self.stampingY_DelayInput = QLineEdit(self)
+        self.stampingY_DelayInput.setGeometry(QRect(300, 75, 50, 25))
+        self.stampingY_DelayInput.setValidator(self.onlyInt)
+        self.stampingY_DelayInput.setText(str(self.stamping_y_delay))
+
+        self.stampingLightOnLabel = QLabel(self)
+        self.stampingLightOnLabel.setGeometry(QRect(200, 100, 100, 25))
+        self.stampingLightOnLabel.setText("Stamping light ON:")
+        self.stampingLightOnInput = QLineEdit(self)
+        self.stampingLightOnInput.setGeometry(QRect(300, 100, 50, 25))
+        self.stampingLightOnInput.setValidator(self.onlyInt)
+        self.stampingLightOnInput.setText(str(self.stamping_light_on))
+
+        self.stampingLightOffLabel = QLabel(self)
+        self.stampingLightOffLabel.setGeometry(QRect(200, 125, 100, 25))
+        self.stampingLightOffLabel.setText("Stamping light OFF:")
+        self.stampingLightOffInput = QLineEdit(self)
+        self.stampingLightOffInput.setGeometry(QRect(300, 125, 50, 25))
+        self.stampingLightOffInput.setValidator(self.onlyInt)
+        self.stampingLightOffInput.setText(str(self.stamping_light_off))
+
+        self.stampingFlushOnLabel = QLabel(self)
+        self.stampingFlushOnLabel.setGeometry(QRect(200, 150, 100, 25))
+        self.stampingFlushOnLabel.setText("Stamping flush ON:")
+        self.stampingFlushOnInput = QLineEdit(self)
+        self.stampingFlushOnInput.setGeometry(QRect(300, 150, 50, 25))
+        self.stampingFlushOnInput.setValidator(self.onlyInt)
+        self.stampingFlushOnInput.setText(str(self.stamping_flush_on))
+
+        self.stampingFlushOffLabel = QLabel(self)
+        self.stampingFlushOffLabel.setGeometry(QRect(200, 175, 100, 25))
+        self.stampingFlushOffLabel.setText("Stamping flush OFF:")
+        self.stampingFlushOffInput = QLineEdit(self)
+        self.stampingFlushOffInput.setGeometry(QRect(300, 175, 50, 25))
+        self.stampingFlushOffInput.setValidator(self.onlyInt)
+        self.stampingFlushOffInput.setText(str(self.stamping_flush_off))
+
+        self.stampingXStepsLabel = QLabel(self)
+        self.stampingXStepsLabel.setGeometry(QRect(200, 200, 100, 25))
+        self.stampingXStepsLabel.setText("Stamping X steps:")
+        self.stampingXStepsInput = QLineEdit(self)
+        self.stampingXStepsInput.setGeometry(QRect(300, 200, 50, 25))
+        self.stampingXStepsInput.setValidator(self.onlyInt)
+        self.stampingXStepsInput.setText(str(self.stamping_x_steps))
+
+        self.stampingYStepsLabel = QLabel(self)
+        self.stampingYStepsLabel.setGeometry(QRect(200, 225, 100, 25))
+        self.stampingYStepsLabel.setText("Stamping Y steps:")
+        self.stampingYStepsInput = QLineEdit(self)
+        self.stampingYStepsInput.setGeometry(QRect(300, 225, 50, 25))
+        self.stampingYStepsInput.setValidator(self.onlyInt)
+        self.stampingYStepsInput.setText(str(self.stamping_y_steps))
+
+        self.stampingBatchSizeLabel = QLabel(self)
+        self.stampingBatchSizeLabel.setGeometry(QRect(200, 250, 100, 25))
+        self.stampingBatchSizeLabel.setText("Stamping Batch size:")
+        self.stampingBatchSizeInput = QLineEdit(self)
+        self.stampingBatchSizeInput.setGeometry(QRect(300, 250, 50, 25))
+        self.stampingBatchSizeInput.setValidator(self.onlyInt)
+        self.stampingBatchSizeInput.setText(str(self.stamping_batch_size))
+
+        # Apply button
+        self.stampingButton = QPushButton(self)
+        self.stampingButton.setGeometry(QRect(200, 300, 100, 30))
+        self.stampingButton.setToolTip("Click to start stamping")
+        self.stampingButton.setText("Stamping ON")
+        self.stampingButton.clicked.connect(self.switch_stamping)
 
         # sfl swithc button
         self.sflButton = QPushButton('SFL ON', self)
@@ -140,10 +248,6 @@ class sflSettingsWindow(QMainWindow):
         self.validateButton.setText("Apply")
         self.validateButton.clicked.connect(self.validate_settings)
 
-        self.message_text = QtWidgets.QPlainTextEdit(self)
-        self.message_text.setGeometry(10, 400, 60, 30)
-        self.message_text.setReadOnly(True)
-        self.message_text.setPlainText("Initialized..")
         # key press event
         # self.keyPressEvent = self.keyPressEvent
 
@@ -160,8 +264,23 @@ class sflSettingsWindow(QMainWindow):
         self.sfl_light_on = int(self.sflLightOnInput.text())
         self.sfl_light_off = int(self.sflLightOffInput.text())
         self.sfl_radius = int(self.sflRadiusInput.text())
+        self.stamping_dx = int(self.stampingDxInput.text())
+        self.stamping_dy = int(self.stampingDyInput.text())
+        self.stamping_x_delay = int(self.stampingX_DelayInput.text())
+        self.stamping_y_delay = int(self.stampingY_DelayInput.text())
+        self.stamping_light_on = int(self.stampingLightOnInput.text())
+        self.stamping_light_off = int(self.stampingLightOffInput.text())
+        self.stamping_flush_on = int(self.stampingFlushOnInput.text())
+        self.stamping_flush_off = int(self.stampingFlushOffInput.text())
+        self.stamping_x_steps = int(self.stampingXStepsInput.text())
+        self.stamping_y_steps = int(self.stampingYStepsInput.text())
+        self.stamping_batch_size = int(self.stampingBatchSizeInput.text())
         self.change_params.emit(self.sfl_light_on, self.sfl_light_off, self.sfl_flush_on, self.sfl_flush_off,
-                                self.sfl_pulse, self.sfl_radius)
+                                self.sfl_pulse, self.sfl_radius, self.stamping_dx, self.stamping_dy, self.stamping_x_delay,
+                                                     self.stamping_y_delay, self.stamping_light_on,
+                                                     self.stamping_light_off, self.stamping_flush_on,
+                                                     self.stamping_flush_off, self.stamping_x_steps,
+                                                     self.stamping_y_steps, self.stamping_batch_size)
 
     def pulse(self):
         self.pulse_signal.emit()
@@ -174,6 +293,14 @@ class sflSettingsWindow(QMainWindow):
 
     def sfl_switch(self):
         self.sfl_switch_signal.emit()
+
+    def switch_stamping(self):
+        if self.stampingButton.text() == "Stamping ON":
+            self.stamping_switch_signal.emit("start")
+            self.stampingButton.setText("Stamping OFF")
+        else:
+            self.stamping_switch_signal.emit("end")
+            self.stampingButton.setText("Stamping ON")
 
 
 class LaserSettingsWindow(QMainWindow):
@@ -475,6 +602,18 @@ class ExampleWindow(QMainWindow):
             self.sfl_light_on = self.config.getint("sfl", "light_on", fallback=50)
             self.sfl_light_off = self.config.getint("sfl", "light_off", fallback=500)
             self.sfl_pulse = self.config.getint("sfl", "pulse", fallback=3000)
+            # stamping settings
+            self.stamping_dx = self.config.getint("stamping", "dx", fallback=1)
+            self.stamping_dy = self.config.getint("stamping", "dy", fallback=1)
+            self.stamping_x_delay = self.config.getint("stamping", "x_delay", fallback=100)
+            self.stamping_y_delay = self.config.getint("stamping", "y_delay", fallback=100)
+            self.stamping_light_on = self.config.getint("stamping", "light_on", fallback=100)
+            self.stamping_light_off = self.config.getint("stamping", "light_off", fallback=100)
+            self.stamping_flush_on = self.config.getint("stamping", "flush_on", fallback=100)
+            self.stamping_flush_off = self.config.getint("stamping", "flush_off", fallback=100)
+            self.stamping_x_steps = self.config.getint("stamping", "x_steps", fallback=100)
+            self.stamping_y_steps = self.config.getint("stamping", "y_steps", fallback=100)
+            self.stamping_batch_size = self.config.getint("stamping", "batch_size", fallback=100)
             print(self.sfl_radius)
         except Exception as ex:
             print(ex)
@@ -838,18 +977,20 @@ class ExampleWindow(QMainWindow):
         self.laser_settings_window.change_params.connect(self.laser_settings_changed)
         self.laser_settings_window.laser_control_signal.connect(self.laser_control)
 
-
-
-
         self.sfl_settings_window = sflSettingsWindow(self.sfl_flush_on, self.sfl_flush_off, self.sfl_light_on,
-                                                         self.sfl_light_off, self.sfl_pulse, self.sfl_radius)
+                                                     self.sfl_light_off, self.sfl_pulse, self.sfl_radius,
+                                                     self.stamping_dx, self.stamping_dy, self.stamping_x_delay,
+                                                     self.stamping_y_delay, self.stamping_light_on,
+                                                     self.stamping_light_off, self.stamping_flush_on,
+                                                     self.stamping_flush_off, self.stamping_x_steps,
+                                                     self.stamping_y_steps, self.stamping_batch_size)
         self.sfl_settings_window.change_params.connect(self.sfl_settings_changed)
 
         self.sfl_settings_window.sfl_switch_signal.connect(self.sfl_switch)
         self.sfl_settings_window.pulse_signal.connect(self.pulse)
         self.sfl_settings_window.flush_switch_signal.connect(self.flush_switch)
         self.sfl_settings_window.light_switch_signal.connect(self.light_switch)
-
+        self.sfl_settings_window.stamping_switch_signal.connect(self.stamping_switch)
         # signal from sfl window
         # self.sfl_settings_window.key_press_signal.connect(self.sfl_key)
 
@@ -902,6 +1043,14 @@ class ExampleWindow(QMainWindow):
         # self.checkboxMarks_group.addButton(self.setLaserCheckbox)
 
         self.showMaximized()
+
+    def stamping_switch(self, command):
+        if command == "start":
+            pass
+        elif command == "end":
+            pass
+        print(command)
+
 
     def keyboardEventReceived(self, event):
         keyboard_pressed = event.name
@@ -1010,19 +1159,47 @@ class ExampleWindow(QMainWindow):
     def laser_control(self, command):
         self.raspi_comm.requests_queue.append(command)
 
-    def sfl_settings_changed(self, light_on, light_off, flush_on, flush_off, pulse, radius):
+    def sfl_settings_changed(self, light_on, light_off, flush_on, flush_off, pulse, radius,
+                             stamping_dx, stamping_dy, stamping_x_delay, stamping_y_delay, stamping_light_on,
+                             stamping_light_off, stamping_flush_on, stamping_flush_off, stamping_x_steps,
+                             stamping_y_steps, stamping_batch_size):
         self.sfl_light_on = light_on
         self.sfl_light_off = light_off
         self.sfl_flush_off = flush_off
         self.sfl_flush_on = flush_on
         self.sfl_pulse = pulse
         self.sfl_radius = radius
+
+        self.stamping_dx = stamping_dx
+        self.stamping_dy = stamping_dy
+        self.stamping_x_delay = stamping_x_delay
+        self.stamping_y_delay = stamping_y_delay
+        self.stamping_light_on = stamping_light_on
+        self.stamping_light_off = stamping_light_off
+        self.stamping_flush_on = stamping_flush_on
+        self.stamping_flush_off = stamping_flush_off
+        self.stamping_x_steps = stamping_x_steps
+        self.stamping_y_steps = stamping_y_steps
+        self.stamping_batch_size = stamping_batch_size
+
         self.config.set("sfl", "pulse", self.sfl_pulse)
         self.config.set("sfl", "light_on", self.sfl_light_on)
         self.config.set("sfl", "light_off", self.sfl_light_off)
         self.config.set("sfl", "flush_on", self.sfl_flush_on)
         self.config.set("sfl", "flush_off", self.sfl_flush_off)
         self.config.set("sfl", "radius", self.sfl_radius)
+
+        self.config.set("stamping", "dx", self.stamping_dx)
+        self.config.set("stamping", "dy", self.stamping_dy)
+        self.config.set("stamping", "x_delay", self.stamping_x_delay)
+        self.config.set("stamping", "y_delay", self.stamping_y_delay)
+        self.config.set("stamping", "light_on", self.stamping_light_on)
+        self.config.set("stamping", "light_off", self.stamping_light_off)
+        self.config.set("stamping", "flush_on", self.stamping_flush_on)
+        self.config.set("stamping", "flush_off", self.stamping_flush_off)
+        self.config.set("stamping", "x_steps", self.stamping_x_steps)
+        self.config.set("stamping", "y_steps", self.stamping_y_steps)
+        self.config.set("stamping", "batch_size", self.stamping_batch_size)
         self.update_config_file()
 
 
