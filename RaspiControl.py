5c9576ef8fc8312a829190cb7b7b0d44d2b92a79
vrbaj <vrbik.vrbik@gmail.com>
2020-10-26 12:32:13 +0100

    raspi control reworked

--- RaspiControl.py
+++ RaspiControl.py
@@ -39,6 +39,7 @@ class RaspiControl:
         GPIO.setup(self.coily_step, GPIO.OUT)
         GPIO.setup(self.shutter, GPIO.OUT)
         GPIO.setup(self.valve, GPIO.OUT)
+        GPIO.setup(self.coil_laser, GPIO.OUT)
         # setup default output values
         GPIO.output(self.valve, GPIO.HIGH)
         GPIO.output(self.shutter, GPIO.LOW)
@@ -163,6 +164,88 @@ class RaspiControl:
         command = 'pkill -9 python'
         os.popen("sudo -S %s" % command, 'w').write('mypass')
 
+    def stamp(self,dx, dy, steps_x, steps_y, delay_x, delay_y, light_on, light_off, sfl_on, sfl_off, batch, stop_pill):
+        for i in range(batch):
+            for j in range(steps_x):
+                print("stepping in x")
+                for k in range(steps_y - 1):
+                    if stop_pill.wait(0.001):
+                        return
+                    print("stepping in y")
+                    # light on
+                    GPIO.output(self.shutter, GPIO.HIGH)
+                    time.sleep(light_on / 1000)
+                    # light off
+                    GPIO.output(self.shutter, GPIO.LOW)
+                    # move in y
+                    GPIO.output(self.coily_enable, GPIO.HIGH)
+                    print("movey")
+                    if dy < 0:
+                        GPIO.output(self.coily_direction, GPIO.LOW)
+                    else:
+                        GPIO.output(self.coily_direction, GPIO.HIGH)
+                    time.sleep(0.05)
+                    for i in range(abs(dy)):
+                        GPIO.output(self.coily_step, 1)
+                        time.sleep(0.001)
+                        GPIO.output(self.coily_step, 0)
+                    time.sleep(delay_y / 1000)
+
+                # light on
+                GPIO.output(self.shutter, GPIO.HIGH)
+                time.sleep(light_on / 1000)
+                # light off
+                GPIO.output(self.shutter, GPIO.LOW)
+                # move in y back
+                time.sleep(light_off / 1000)
+                print("moving back in y")
+                GPIO.output(self.coily_enable, GPIO.HIGH)
+                print("movey")
+                steps = -1 * dy * (steps_y - 1)
+                if steps < 0:
+                    GPIO.output(self.coily_direction, GPIO.LOW)
+                else:
+                    GPIO.output(self.coily_direction, GPIO.HIGH)
+                time.sleep(0.05)
+                for i in range(abs(steps)):
+                    GPIO.output(self.coily_step, 1)
+                    time.sleep(0.001)
+                    GPIO.output(self.coily_step, 0)
+                time.sleep(delay_y / 1000)
+                # move dx
+                print("moving in x")
+                GPIO.output(self.coilx_enable, GPIO.HIGH)
+                if j < steps_x - 1:
+                    print("movex")
+                    if dx < 0:
+                        GPIO.output(self.coilx_direction, GPIO.LOW)
+                    else:
+                        GPIO.output(self.coilx_direction, GPIO.HIGH)
+                    time.sleep(0.05)
+                    for i in range(abs(dx)):
+                        GPIO.output(self.coilx_step, 1)
+                        time.sleep(0.001)
+                        GPIO.output(self.coilx_step, 0)
+                    time.sleep(delay_x / 1000)
+            # return to start
+            print("return to start position")
+            GPIO.output(self.coilx_enable, GPIO.HIGH)
+            print("movex")
+            steps = dx * -1 * (steps_x - 1)
+            if steps < 0:
+                GPIO.output(self.coilx_direction, GPIO.LOW)
+            else:
+                GPIO.output(self.coilx_direction, GPIO.HIGH)
+            time.sleep(0.05)
+            for i in range(abs(steps)):
+                GPIO.output(self.coilx_step, 1)
+                time.sleep(0.001)
+                GPIO.output(self.coilx_step, 0)
+            time.sleep(delay_y / 1000)
+            # flush on
+            print("flush on")
+            GPIO.output(self.valve, GPIO.LOW)
+
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.HOST, self.PORT))
@@ -176,11 +259,11 @@ class RaspiControl:
                             data_decode = data.decode("UTF-8")
                             self.requests_list.extend(filter(None, data_decode.split(";")))
                     except Exception as ex:
-                        if len(requests_list) == 0:
+                        if len(self.requests_list) == 0:
                             break
 
                     if len(self.requests_list) > 0:
-                        decoded = requests_list.pop()
+                        decoded = self.requests_list.pop()
                         if decoded[0] == "x":
                             self.move_x(steps=int(decoded[1:]))
                         elif decoded[0] == "y":
@@ -199,43 +282,43 @@ class RaspiControl:
                         elif decoded[0] == "t":
                             self.stop_blink_thread
                         elif decoded[0] == "r":
-                            stop_thread.set()
-                            stop_thread2.set()
-                            stop_thread3.set()
+                            self.stop_blink_thread.set()
+                            self.stop_sfl_thread.set()
+                            self.stop_stamping_thread.set()
                             time.sleep(0.1)
-                            requests_list = []
-                            red_button(used_pins)
+                            self.requests_list = []
+                            self.red_button()
                             # TODO: red button. set all outputs to LOW, terminate laser thread
                         elif decoded[0] == "q":
                             on_time = int(decoded.split(",")[1])
                             off_time = int(decoded.split(",")[2])
                             n = int(decoded.split(",")[3])
-                            blink_n(on_time, off_time, coil_laser, n)
+                            self.blink_n(on_time, off_time, n)
                         elif decoded[0] == "p":
                             flush_on = int(decoded.split(",")[1])
                             flush_off = int(decoded.split(",")[2])
                             light_on = int(decoded.split(",")[3])
                             light_off = int(decoded.split(",")[4])
-                            stop_thread2.clear()
-                            my_thread2 = threading.Thread(target=sfl,
+                            self.stop_sfl_thread.clear()
+                            self.sfl_thread = threading.Thread(target=self.sfl,
                                                           args=(
-                                                          flush_on, flush_off, light_on, light_off, valve, shutter,
-                                                          stop_thread2))
-                            my_thread2.start()
+                                                          flush_on, flush_off, light_on, light_off,
+                                                          self.stop_sfl_thread))
+                            self.sfl_thread.start()
                         elif decoded[0] == "h":
-                            GPIO.output(shutter, GPIO.LOW)
+                            GPIO.output(self.shutter, GPIO.LOW)
                         elif decoded[0] == "j":
-                            GPIO.output(shutter, GPIO.HIGH)
+                            GPIO.output(self.shutter, GPIO.HIGH)
                         elif decoded[0] == "n":
-                            GPIO.output(valve, GPIO.LOW)
+                            GPIO.output(self.valve, GPIO.LOW)
                         elif decoded[0] == "m":
-                            GPIO.output(valve, GPIO.HIGH)
+                            GPIO.output(self.valve, GPIO.HIGH)
                         elif decoded[0] == "a":
                             on_time = int(decoded.split(",")[1])
                             wait_time = int(decoded.split(",")[2])
-                            one_pulse(on_time, wait_time, shutter)
+                            self.one_pulse(on_time, wait_time)
                         elif decoded[0] == "c":
-                            stop_thread3.set()
+                            self.stop_stamping_thread.set()
                         elif decoded[0] == "b":
                             # batch
                             dx = int(decoded.split(",")[1])
@@ -249,11 +332,17 @@ class RaspiControl:
                             sfl_on = int(decoded.split(",")[9])
                             sfl_off = int(decoded.split(",")[10])
                             batch = int(decoded.split(",")[11])
-                            stop_thread3.clear()
-                            my_thread3 = threading.Thread(target=stamp,
+                            self.stop_stamping_thread.clear()
+                            self.stamping_thread = threading.Thread(target=self.stamp,
                                                           args=(dx, dy, steps_x, steps_y, delay_x, delay_y, light_on,
                                                                 light_off,
-                                                                sfl_on, sfl_off, batch, stop_thread3))
-                            my_thread3.start()
+                                                                sfl_on, sfl_off, batch, self.stop_stamping_thread))
+                            self.stamping_thread.start()
                         data = None
             GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    raspi = RaspiControl(HOST, PORT,DELAY)
+    raspi.run()
+    print("exiting...")
